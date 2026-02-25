547cdf4f10eef6896044e349064ebc5d3d4f8d4f
Stephen Ellis <steellis98@gmail.com>
2026-02-25 14:43:27 +0000

    Added seed to initialise exercises table

---
+++ seed.sql
@@ -0,0 +1,17 @@
+USE exercise;
+
+INSERT INTO exercises (name, category, body_part)
+VALUES ('Running', 'Cardio','Legs'),
+       ('Pushups', 'Strength','Upper body'),
+       ('Left side plank', 'Strength','Core'),
+       ('Right side plank', 'Strength','Core'),
+       ('Plank', 'Strength','Core'),
+       ('Chair dips', 'Strength','Arms'),
+       ('Squats', 'Strength','Legs'),
+       ('Jumping squats', 'Strength','Legs'),
+       ('Glute bridges', 'Strength','Legs'),
+       ('Right lunges', 'Strength','Legs'),
+       ('Left lunges', 'Strength','Legs'),
+       ('Star Jumps', 'Cardio','Legs and Arms'),
+       ('Walking', 'Cardio','Legs'),
+       ('Rock climbing', 'Strength','Full Body');
