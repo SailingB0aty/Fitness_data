1e737f337c2cff67edf86332bc9934c4986df2e3
Stephen Ellis <steellis98@gmail.com>
2026-02-27 14:37:29 +0000

    Add workout and add exercise tabs now work to insert data into the database

--- web_app.py
+++ web_app.py
@@ -1,8 +1,12 @@
+import math
+
 import streamlit as st
 import pandas as pd
 import datetime as dt
+import time
 import os
 from narwhals.sql import table
+from pyarrow import duration
 from sqlalchemy import create_engine, text
 from dotenv import load_dotenv
 
@@ -45,16 +49,27 @@ with engine.connect() as conn:
 tab1.dataframe(df, height='content', width='stretch')
 
 # ~~~ Tab 2 widgets ~~~ #
-exercise_count = tab2.number_input("How many exercises?", 1, 10)
+if "last_sql" not in st.session_state:
+    st.session_state.last_sql = ""
+
+if st.session_state.get("_reset_e_count", False):
+    st.session_state.e_count = 1
+    st.session_state._reset_e_count = False
+
+exercise_count = tab2.empty()
+e_count = exercise_count.number_input("How many exercises?", 1, 10, key="e_count")
+
 with tab2.form("New Workout", clear_on_submit=True, enter_to_submit=False, border=True, width="stretch", height="content"):
     workout_date = st.date_input("Workout date", value=dt.date.today())
     workout_time = st.time_input("Workout start time")
     workout_duration = st.number_input("Workout duration (Mins)", 1, 600)
+    # Return "NULL" for notes if blank or just spaces
     workout_notes = st.text_input("Workout notes")
+    notes = "NULL" if workout_notes.strip() == "" else '"' + workout_notes + '"'
 
     st.markdown("### Exercises ###")
     items = []
-    for i in range(exercise_count):
+    for i in range(e_count):
         c1, c2, c3, c4 = st.columns([3,1,1,1])
 
         # Create widgets with a key so they can be distinguised from other widgets in the loop
@@ -65,11 +80,62 @@ with tab2.form("New Workout", clear_on_submit=True, enter_to_submit=False, borde
 
         items.append({
             "exercise_id": exercise[0],
-            "reps": None if reps == 0 else reps,
-            "sets": None if sets == 0 else sets,
-            "distance": None if distance == 0 else distance
+            "reps": "NULL" if reps == 0 else reps,
+            "sets": "NULL" if sets == 0 else sets,
+            "distance": "NULL" if distance == 0 else distance
         })
 
     submitted = st.form_submit_button("Submit")
+    if submitted:
+        # Must convert duration to correct format
+        duration_formatted = time.strftime('%H:%M:%S', time.gmtime(workout_duration*60))
+        # Create SQL to add workout
+        sql = (f"USE exercise;"
+               f"INSERT INTO workouts(workout_date, duration, notes)\n"
+               f"VALUES ('{workout_date}', '{duration_formatted}', {notes});\n"
+               f"SET @workout_id = LAST_INSERT_ID();\n"
+               f"INSERT INTO workout_items(workout_id, exercise_id, reps, sets, distance)\n"
+               f"VALUES")
+        for item in items:
+            sql += f"\n(@workout_id, {item["exercise_id"]}, {item["reps"]}, {item["sets"]}, {item["distance"]}),"
+        sql = sql[:-1]
+        sql += ";"
+
+        # Reset exercise count and persist sql
+        st.session_state.last_sql = sql
+        st.session_state._reset_e_count = True
+
+        # Send the SQL to MySQl
+        with engine.begin() as conn:
+            conn.execute(text(sql))
+
+        st.rerun()
+
+    # Show last SQL entry
+    if st.session_state.last_sql:
+        tab2.text_area("Last SQL Submitted:", st.session_state.last_sql, height='content')
 
 # ~~~ Tab 3 widgets ~~~ #
+with tab3.form("New Exercise", clear_on_submit=True, enter_to_submit=False, border=True, width="stretch", height="content"):
+    c1, c2, c3 = st.columns([3, 1, 1])
+    name = c1.text_input("Exercise Name")
+    # NOTE I should un hardcode this
+    category = c2.selectbox("Category", ["Cardio", "Strength"])
+    body_part = c3.selectbox("Body Part", ["Legs", "Upper Body", "Core", "Arms", "Full body"])
+    submitted = st.form_submit_button("Submit")
+
+    if submitted:
+        # Create SQL to add workout
+        sql = (f"USE exercise;\n"
+               f"INSERT INTO exercises(name, category, body_part)\n"
+               f"VALUE ('{name}', '{category}', '{body_part}');")
+
+        st.session_state.last_sql = sql
+
+        # Send the SQL to MySQl
+        with engine.begin() as conn:
+            conn.execute(text(sql))
+
+        # Show last SQL entry
+        if st.session_state.last_sql:
+            tab3.text_area("Last SQL Submitted:", st.session_state.last_sql, height='content')
